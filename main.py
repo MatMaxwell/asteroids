6527e6b085da9851e508fede2e81cf8d38600ce1
Matthew Maxwell <obliskie@MatthewsMSI.localdomain>
2025-07-02 20:31:46 -0500

    Adding the tick rate

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from constants import *
 
 def main():
     pygame.init()
+    fps = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
     while running:
@@ -14,6 +16,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        tick = fps.tick(60)
+        dt = tick / 1000
 
 
 if __name__ == "__main__":
